a50e36f383c4b3fb93e1d451a5c48aae5ad5adc6
Adrian-java <arembisz19@gmail.com>
2016-02-07 11:32:30 +0100

    second

--- server/server.js
+++ server/server.js
@@ -1,21 +1,37 @@
 /* globals require, setTimeout, setInterval, console, __dirname */
 
-/**
- * API: GET /contact - returns object GET /menu - returns array of objects, each
- * containing id, name, ingredients and price POST /order - post order in form
- * of array of object containing id (of the pizza) and quantity. Invalid id or
- * quantity <= 0 will return 500 error GET /order/:id - returns object with
- * order, ordered (time when order was processed) and estimated (time of
- * arrival)
+/** API:
+ *  GET /contact - returns object
+ *  GET /menu - returns array of objects, each containing id, name, ingredients (their ids) and price
+ *  GET /ingredients - returns array of object, each containing id, name and (extra) price
+ *  GET /extras - returns array of objects, each containing id, name and price
+ *  POST /order - post order in form of object.
+ *                Property order must be an array of object containing id (of the pizza) and quantity.
+ *                Property extras must be an array of object containing id (of the extras) and quantity.
+ *                Property orderInfo must be an object containing orders info.
+ *  GET /order/:id - returns object with order, ordered (time when order was processed) and estimated (time of arrival)
  */
-var express = require('express'), bodyParser = require('body-parser'), _ = require('lodash'), menu = require('./menu');
+var express = require('express'),
+    webSocketServer = require('websocket').server,
+    bodyParser = require('body-parser'),
+    _ = require('lodash'),
+    data = {
+        menu: require('./data/menu'),
+        ingredients: require('./data/ingredients'),
+        extras: require('./data/extras')
+    };
 
-var app = express(), orders = {}, i = 0;
 
+var app = express(),
+    orders = {},
+    i = 0;
+var httpServer, wsServer, wsConnections = [];
+
+app.use(express.static(__dirname + '/../build/'));
 app.use(express.static(__dirname + '/../client/'));
 
 app.use(bodyParser.urlencoded({
-	extended : false
+    extended: false
 }));
 app.use(bodyParser.text());
 app.use(bodyParser.json());
@@ -26,101 +42,162 @@ app.get('/', function(req, res) {
 	}, _.random(100, 1500));
 });
 
-app.get('/contact', function(req, res) {
-	setTimeout(function() {
-		res.json({
-			name : 'Pizza',
-            phone : '881239402',
-			hours : 'caly dzien',
-			address : {
-				street : 'jasna 20',
-				city : 'Wrocław'
-			}
-			
-		});
-	}, _.random(100, 1500));
+app.get('/contact', function (req, res) {
+    setTimeout(function () {
+        res.json({
+            name: 'Pizzeria',
+            address: {
+                street: 'Św. Marcin 1',
+                city: 'Poznań'
+            },
+            phone: '123-456-789',
+            hours: '12:00 - 22:00'
+        });
+    }, _.random(100, 1500));
 });
 
-app.get('/menu', function(req, res) {
-	setTimeout(function() {
-		res.json(menu);
-	}, _.random(100, 1500));
+app.get('/menu', function (req, res) {
+    setTimeout(function () {
+        res.json(data.menu);
+    }, _.random(100, 1500));
+});
+
+app.get('/ingredients', function (req, res) {
+    setTimeout(function () {
+        res.json(data.ingredients);
+    }, _.random(100, 1500));
+});
+
+app.get('/extras', function (req, res) {
+    setTimeout(function () {
+        res.json(data.extras);
+    }, _.random(100, 1500));
 });
 
 /**
- * Checks if pizza in each position exists and its quantity is greater than 0
- * 
- * @param {object}
- *            order Requests body
- * @returns {boolean} Is order valid
+ * Checks if request is valid
+ * @param   {object} order Requests body
+ * @returns {boolean}      Is order valid
  */
-function checkIsOrderValid(order) {
- console.log('Order recived', order instanceof Array);
-	var isArray = order instanceof Array, areItemsValid = _.every(order,
-			function(position) {
-        console.log('position',position);
-				var isQuantityValid = _.isNumber(position.quantity)
-						&& position.quantity > 0, isValidPizza = _
-						.isNumber(position.id)
-						&& !_.isEmpty(_.find(menu, {
-							id : position.id
-						}));
+function checkIsOrderValid(reqData) {
+    var isOrderAnArray = reqData.order instanceof Array,
+        areOrderItemsValid = isListValid(reqData.order, data.menu),
+        areExtrasAnArray = reqData.extras instanceof Array,
+        areExtrasValid = isListValid(reqData.extras, data.extras),
+        isOrderInfoAnObject = reqData.orderInfo instanceof Object;
 
-				return isQuantityValid && isValidPizza;
-			});
+    function isListValid (listToCheck, entities) {
+        return _.every(listToCheck, function (position) {
+            var isQuantityValid = _.isNumber(position.quantity) && position.quantity > 0,
+                isValidEntity = _.isNumber(position.id) && !_.isEmpty(_.find(entities, {
+                    id: position.id
+                }));
 
-	return isArray && areItemsValid;
+            return isQuantityValid && isValidEntity;
+        });
+    }
+
+    return isOrderAnArray & areOrderItemsValid & areExtrasAnArray & areExtrasValid & isOrderInfoAnObject;
 }
 
-app.post('/order', function(req, res) {
-	setTimeout(function() {
-		console.log('Order recived', req.body);
-		if (checkIsOrderValid(req.body)) {
-			var now = Date.now();
-            console.log('jest');
-			orders[i] = {
-				order : req.body.map(function(position) {
-					return {
-						pizza : _.find(menu, {
-							id : position.id
-						}),
-						quantity : position.quantity
-					};
-				}),
-				ordered : new Date(now),
-				estimated : new Date(now + _.random(15, 90))
-			};
-console.log('jest',orders[i]);
-			res.json({
-				id : i
-			});
-         console.log('jest');
-			i++;
-		} else {
-            console.log('error');
-			res.status(500).send('Invalid order.');
-		}
-	}, _.random(100, 1500));
+/**
+ * Update all connected WS clients
+ * @param {number} id
+ * @param {object} order
+ */
+function updateClients(id, order) {
+    _.forEach(wsConnections, function(connection) {
+        connection.sendUTF(JSON.stringify({
+            id: id,
+            order: order
+        }));
+    });
+}
+
+app.post('/order', function (req, res) {
+    setTimeout(function () {
+        console.log('Order recived', req.body);
+        if (checkIsOrderValid(req.body)) {
+            var now = Date.now();
+
+            orders[i] = {
+                order: {
+                    order: req.body.order.map(function (position) {
+                        return {
+                            pizza: {
+                                id: position.id,
+                                extraIngredients: position.extraIngredients
+                            },
+                            quantity: position.quantity
+                        };
+                    }),
+                    extras: req.body.extras
+                },
+                orderInfo: req.body.orderInfo,
+                ordered: new Date(now),
+                estimated: new Date(now + _.random(30, 120) * 10000),
+                status: 0
+            };
+            console.log('Order status: 0');
+
+            res.json({
+                id: i
+            });
+
+            i++;
+        } else {
+            res.status(500).send('Invalid order.');
+            console.error('Invalid order!');
+        }
+    }, _.random(100, 1500));
 });
 
-app.get('/order/:id', function(req, res) {
-	setTimeout(function() {
-		if (orders[req.params.id] === undefined) {
-			res.status(404).send('Order does not exist.');
-		} else {
-			res.json(orders[req.params.id]);
-		}
-	}, _.random(100, 1500));
+app.get('/order/:id', function (req, res) {
+    setTimeout(function () {
+        if (orders[req.params.id] === undefined) {
+            res.status(404).send('Order does not exist.');
+        } else {
+            res.json(orders[req.params.id]);
+        }
+    }, _.random(100, 1500));
 });
 
-setInterval(function() {
-	_.forEach(orders, function(order, id) {
-		if (order.ordered > new Date()) {
-			delete orders[id];
-		}
-	});
-}, 300);
+setInterval(function () {
+    _.forEach(orders, function (order, id) {
+        var diff = order.estimated - order.ordered;
 
-var server = app.listen(8080, function() {
-	console.log('Server running on port 8080.');
+        if (order.status < 1 && order.ordered * 1 + diff * 0.2 <= Date.now()) {
+            order.status = 1;
+            console.log('Order ' + id + ' changed status to 1');
+            updateClients(id, order);
+        }
+        if (order.status < 2 && order.ordered * 1 + diff * 0.5 <= Date.now()) {
+            order.status = 2;
+            console.log('Order ' + id + ' changed status to 2');
+            updateClients(id, order);
+        }
+        if (order.status < 3 && order.estimated <= Date.now()) {
+            order.status = 3;
+            console.log('Order ' + id + ' changed status to 3');
+            updateClients(id, order);
+        }
+    });
+}, 500);
+
+httpServer = app.listen(8080, function () {
+    console.log('Server running on port 8080.');
+});
+wsServer = new webSocketServer({
+    httpServer: httpServer
+});
+
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+    var index = wsConnections.push(connection);
+
+    console.log('WebSocket Client connected on ' + new Date());
+
+    connection.on('close', function(con) {
+        wsConnections.slice(index, 1);
+    });
 });
